add5eacbccbd16ee18afeaf9f774c2eba12316e6
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-05-29 21:37:09 -0400

    ci: add gitHub pages workflow

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -4,6 +4,7 @@ import preact from '@preact/preset-vite';
 export default defineConfig({
   plugins: [preact()],
   root: './demo',
+  base: process.env.PUBLIC_BASE_URL || process.env.BASE_URL || '/',
   server: {
     port: 3000,
   },
